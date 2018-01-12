aaf6e1b0dba0afcafd5687090e73a9f12885f8c4
Andrew McGillivray <ap.mcgillivray@gmail.com>
2018-01-12 22:09:02 +1100

    add api call

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
 
+//typed routes/api
+const (
+	base         string = "/"
+	sub          string = "/sub"
+	endpoint     string = "https://api.dribbble.com/v1/shots"
+	test_rebound string = "/3850371"
+	access       string = "?access_token=859f432cad2e206603ed29d9c5728f0c14673e2ea14c453b07f3b05a36cd2fea"
+)
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, port())
+	fmt.Fprintf(w, "base")
 }
 
 func subHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,8 +32,21 @@ func port() string {
 	return ":" + port
 }
 
+func fetchData() {
+	resp, err := http.Get(endpoint + access)
+	if err != nil {
+		fmt.Println(err)
+	}
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	data := string(bytes)
+	fmt.Println(data)
+
+	resp.Body.Close()
+}
+
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/about", subHandler)
+	fetchData()
+	http.HandleFunc(base, indexHandler)
+	http.HandleFunc(sub, subHandler)
 	http.ListenAndServe(port(), nil)
 }
